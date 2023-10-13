afa034ef6dd9a90ad9d6c042e896e49872a43ea7
Miguel Remedios <miguelremediios@gmail.com>
2023-10-13 02:18:34 +0100

    navbar fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 
