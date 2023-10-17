c26c3e07d2d049fca417ac56fcf59702cd38149b
Miguel Remedios <miguelremediios@gmail.com>
2023-10-17 17:43:52 +0100

    change folder structure and components

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import HeroBgAnimation from "../HeroBgAnimation";
+import {
+  HeroContainer,
+  HeroBg,
+  HeroLeftContainer,
+  Img,
+  HeroRightContainer,
+  HeroInnerContainer,
+  TextLoop,
+  Title,
+  Span,
+  SubTitle,
+  ResumeButton,
+  TitleMini,
+} from "./HeroStyle";
+import HeroImg from "../../images/remedios-yellow.png";
+import Typewriter from "typewriter-effect";
+import { Bio } from "../../data/constants";
+
+const HeroSection = () => {
+  return (
+    <div id="about">
+      <HeroContainer>
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
+        <HeroInnerContainer>
+          <HeroLeftContainer id="Left">
+            <TitleMini>Hi there! I am</TitleMini>
+            <Title>{Bio.name}</Title>
+            <TextLoop>
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle>{Bio.description}</SubTitle>
+            <ResumeButton href={Bio.resume} target="display">
+              Contact Me
+            </ResumeButton>
+          </HeroLeftContainer>
+
+          <HeroRightContainer id="Right">
+            <Img src={HeroImg} alt="miguel-remedios-image" />
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+};
+
+export default HeroSection;
